601c88de02336009e23b7709f1527d5f31bec072
baiyongrui <baiyongrui@icloud.com>
2025-04-03 09:13:55 +0800

    Avoid double copy in collecting parameters, when finetuning larger model will easily cause OOM

--- src/training/train_utils.py
+++ src/training/train_utils.py
@@ -3,18 +3,23 @@ import torch
 import logging
 
 
-def maybe_zero_3(param, ignore_status=False, name=None):
+def maybe_zero_3(param, ignore_status=False, name=None, device=torch.device('cpu')):
     from deepspeed import zero
     from deepspeed.runtime.zero.partition_parameters import ZeroParamStatus
+    if type(device) is str:
+        device = torch.device(device)
     if hasattr(param, "ds_id"):
         if param.ds_status == ZeroParamStatus.NOT_AVAILABLE:
             if not ignore_status:
                 logging.warning(f"{name}: param.ds_status != ZeroParamStatus.NOT_AVAILABLE: {param.ds_status}")
         with zero.GatheredParameters([param]):
-            param = param.data.detach().cpu().clone()
+            param = param.data.detach()
     else:
-        param = param.detach().cpu().clone()
-    return param
+        param = param.detach()
+    if device == param.device:
+        return param.clone()
+    else:
+        return param.to(device)
 
 # Borrowed from peft.utils.get_peft_model_state_dict
 def get_peft_state_maybe_zero_3(named_params, bias):
@@ -46,7 +51,7 @@ def get_peft_state_non_lora_maybe_zero_3(named_params, require_grad_only=True):
     to_return = {k: t for k, t in named_params if "lora_" not in k}
     if require_grad_only:
         to_return = {k: t for k, t in to_return.items() if t.requires_grad}
-    to_return = {k: maybe_zero_3(v, ignore_status=True).cpu() for k, v in to_return.items()}
+    to_return = {k: maybe_zero_3(v, ignore_status=True) for k, v in to_return.items()}
     return to_return
 
 def safe_save_model_for_hf_trainer(trainer: transformers.Trainer,
