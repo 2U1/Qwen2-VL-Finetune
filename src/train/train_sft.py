fix: Disable reentrant gradient checkpointing for improved stability

src/train/train_sft.py
@@ -145,11 +145,11 @@ def train():
     if training_args.bits in [4,8]:
         model.config.torch_dtype = (torch.float32 if training_args.fp16 else (torch.bfloat16 if training_args.bf16 else torch.float32))
         from peft import prepare_model_for_kbit_training
-        model = prepare_model_for_kbit_training(model, use_gradient_checkpointing=training_args.gradient_checkpointing, gradient_checkpointing_kwargs={"use_reentrant": True})
+        model = prepare_model_for_kbit_training(model, use_gradient_checkpointing=training_args.gradient_checkpointing, gradient_checkpointing_kwargs={"use_reentrant": False})
     
     if training_args.gradient_checkpointing:
         model.enable_input_require_grads()
-        training_args.gradient_checkpointing_kwargs = {"use_reentrant": True}
+        training_args.gradient_checkpointing_kwargs = {"use_reentrant": False}
 
     if training_args.lora_enable:
         lora_namespan_exclude = training_args.lora_namespan_exclude